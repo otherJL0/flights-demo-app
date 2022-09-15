5874a4745290b0b9462daa694ba93b979c9ed131
Jonathan Lopez <jonathanglopez@gmail.com>
2022-09-14 20:05:34 -0400

    Remove uvicorn from main.py

--- backend/flightsapi/main.py
+++ backend/flightsapi/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello world"}
-
-
-def main():
-    uvicorn.run("flightsapi.main:app", host="127.0.0.1", port=5000, log_level="info")
